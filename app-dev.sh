f3cc32d71e9415385d911422f81f26308912675c
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2020-05-09 14:25:34 +0900

    add optional argument

--- app-dev.sh
+++ app-dev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 環境変数の読み込み
-. .dev.env
+. dev.env
 # 環境変数が未定義の場合、処理中断
 if [[ -z ${NODE_PATH} || -z ${KURUMI_MAIL_PATH} || -z ${PADDLER} || -z ${FISH_DISH} ]]; then
     echo '- 環境変数が無効です.. '
     exit 1
 fi
-# 実行
-${NODE_PATH} ${PADDLER} ${KURUMI_MAIL_PATH} emlx | xargs ${NODE_PATH} ${FISH_DISH}
+# 実行（`paddler`のオプション`-T`はテストモード。`walnut99.emlx`を必ず検出する）
+${NODE_PATH} ${PADDLER} ${KURUMI_MAIL_PATH} emlx -T | xargs ${NODE_PATH} ${FISH_DISH} -s='Yahoo! JAPAN - 確認コード'
--- app-prod.sh
+++ app-prod.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 環境変数の読み込み
-. .prod.env
+. prod.env
 # 環境変数が未定義の場合、処理中断
 if [[ -z ${KURUMI_MAIL_PATH} || -z ${PADDLER} || -z ${FISH_DISH} ]]; then
     echo '- 環境変数が無効です.. '
     exit 1
 fi
-# 実行
-${PADDLER} ${KURUMI_MAIL_PATH} emlx | xargs ${FISH_DISH}
+# 実行（`pkg`による、単一ファイル化版）
+${PADDLER} ${KURUMI_MAIL_PATH} emlx | xargs ${FISH_DISH} -s='Yahoo! JAPAN - 確認コード'
